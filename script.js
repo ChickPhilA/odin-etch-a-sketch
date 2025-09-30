3c537aaf0282df4a9e0955eb016972d026e326f1
Phil <natsudragoneel282003@gmail.com>
2025-09-30 09:29:26 -0700

    made a 16x16 grid instead and removed the border that surrounded each row

--- script.js
+++ script.js
@@ -1,11 +1,11 @@
 let container = document.querySelector('.container')
 
-for(let i = 0; i < 64; i++) {
+for(let i = 1; i < 16; i++) {
     let row = document.createElement('div')
     row.classList.add('row')
     container.appendChild(row)
     
-    for(let j = 1; j <= 64; j++) {
+    for(let j = 1; j <= 16; j++) {
         let square = document.createElement('div')
         square.classList.add('square')
         row.appendChild(square)
