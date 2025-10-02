e17fac267e5b2fcf3bde7f98c3fffd1960035998
Phil <natsudragoneel282003@gmail.com>
2025-10-02 11:49:29 -0700

    applied flex-grow to all child container elements to grow within container size

--- script.js
+++ script.js
@@ -1,5 +1,11 @@
 let container = document.querySelector('.container')
 
+/* 
+ This will be the code for creating a nxn grid of squares.
+*/
+
+// let size = prompt("Enter the number of squares per side for the grid (max 100): ")
+
 for(let i = 1; i <= 16; i++) {
     let row = document.createElement('div')
     row.classList.add('row')
