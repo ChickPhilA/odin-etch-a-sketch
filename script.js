14f10fb68130fa6408475039d7b4387bf7f5a4aa
Phil <natsudragoneel282003@gmail.com>
2025-10-02 15:39:56 -0700

    created fully functioning etch-a-sketch, may choose to do opacity version in the future

--- script.js
+++ script.js
@@ -1,10 +1,8 @@
 let container = document.querySelector('.container')
+let sizeButton = document.querySelector('#sizeButton')
+let clearButton = document.querySelector('#clearButton')
+let num = 16;
 
-/* 
- This will be the code for creating a nxn grid of squares.
-*/
-
-// let size = prompt("Enter the number of squares per side for the grid (max 100): ")
 
 for(let i = 1; i <= 16; i++) {
     let row = document.createElement('div')
@@ -22,3 +20,42 @@ for(let i = 1; i <= 16; i++) {
         })
     }
 }
+
+function resizeGrid(num) {
+    num = prompt("Enter new grid size (max 100): ")
+    if(num < 1 || num > 100) {
+        alert("Invalid input. Please enter a number between 1 and 100.")
+        return
+    }
+
+    container.remove()
+    container = document.createElement('div')
+    container.classList.add('container')
+    document.body.appendChild(container)
+
+    for(let i = 1; i <= num; i++) {
+        let row = document.createElement('div')
+        row.classList.add('row')
+        container.appendChild(row)
+        
+        for(let j = 1; j <= num; j++) {
+            let square = document.createElement('div')
+            square.classList.add('square')
+            row.appendChild(square)
+
+            square.addEventListener('mouseover', () => {
+                square.style.backgroundColor = 'black'
+            })
+        }
+    }
+}
+
+function cleanGrid() {
+    let squares = document.querySelectorAll('.square')
+    squares.forEach((square) => {
+        square.style.backgroundColor = 'white'
+    })
+}
+
+sizeButton.addEventListener('click', () => resizeGrid(num))
+clearButton.addEventListener('click', () => cleanGrid())
